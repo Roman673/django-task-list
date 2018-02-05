e0b7c2579b6ff6dccfc9fe587d9e9e0145000f51
Roma <roma2@protonmail.com>
2018-02-05 16:48:15 +0300

    Created REST API

--- tasks/api/urls.py
+++ tasks/api/urls.py
@@ -2,10 +2,18 @@ from django.urls import path
 
 from .views import (
     TaskListAPIView,
+    TaskCreateAPIView,
+    TaskDestroyAPIView,
+    TaskRetrieveAPIView,
+    TaskUpdateAPIView,
     )
 
 app_name = 'tasks-api'
 
 urlpatterns = [
-    path('', TaskListAPIView.as_view()),
+    path('', TaskListAPIView.as_view(), name="tasks-list"),
+    path('create/', TaskCreateAPIView.as_view(), name="task-create"),
+    path('<int:pk>/detail/', TaskRetrieveAPIView.as_view(), name="task-detail"),
+    path('<int:pk>/update/', TaskUpdateAPIView.as_view(), name="task-update"),
+    path('<int:pk>/delete/', TaskDestroyAPIView.as_view(), name="task-delete"),
 ]
--- tasks/api/views.py
+++ tasks/api/views.py
@@ -1,4 +1,10 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import (
+        ListAPIView,
+        CreateAPIView,
+        RetrieveAPIView,
+        RetrieveUpdateAPIView,
+        RetrieveDestroyAPIView,
+    )
 
 from ..models import Task
 from .serializers import TaskSerializer
@@ -8,3 +14,21 @@ class TaskListAPIView(ListAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+
+class TaskCreateAPIView(CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDestroyAPIView(RetrieveDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskRetrieveAPIView(RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
